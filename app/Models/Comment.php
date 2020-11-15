85587f1dfb46f8392ed666fd7cf2f9de0735731d
kai0310 <kn0310@outlook.jp>
2020-11-15 23:22:00 +0900

    モデル, マイグレーションファイルの作成

---
+++ app/Models/Comment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Post;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'text',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'title',
+      'body',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(): hasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
