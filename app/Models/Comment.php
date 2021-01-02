497e970d2d902d0b6f866c08d4cd03a3d9461999
kai0310 <kn0310@outlook.jp>
2021-01-02 10:12:27 +0900

    fix: bug can't see comments of unsubscribed users

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -36,7 +36,7 @@ class Comment extends Model
      */
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     /**
