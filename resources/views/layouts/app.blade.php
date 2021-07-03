fb0647dd94985ae3c13744a680bded644a285b7e
kai0310 <kn0310@outlook.jp>
2021-07-03 22:41:58 +0900

    fix: error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-dropdown')
-
+            @livewire('navigation-menu')
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 select-none">
