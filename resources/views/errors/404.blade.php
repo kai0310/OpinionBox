b1961271ee1c9dc2f3302c37600cfff7374ff8d4
kai0310 <kn0310@outlook.jp>
2021-07-16 20:54:49 +0900

    feat: create error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('errors::illustrated-layout')
+
+@section('title', __('Not Found'))
+@section('code', '404')
+@section('message', 'このページはご利用いただけません')
+
+@section('image')
+    <div
+        style="background-image: url({{ asset('/icons/not_found.png') }});"
+        class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center"
+    ></div>
+@endsection
+
+@section('detail')
+    アクセスしようとしたページが見つかりませんでした。
+    ページが移動または削除されたか、URLの入力間違いの可能性があります。
+@endsection
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,16 @@
+@extends('errors::illustrated-layout')
+
+@section('title', __('Service Unavailable'))
+@section('code', '503')
+@section('message', '現在、サービスはご利用いただけません')
+
+@section('image')
+    <div
+        style="background-image: url({{ asset('/icons/sleeping.png') }});"
+        class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center"
+    ></div>
+@endsection
+
+@section('detail')
+    サービスは、メンテナンス中のためご利用いただけません。しばらくたってから、再度お試しください。
+@endsection
