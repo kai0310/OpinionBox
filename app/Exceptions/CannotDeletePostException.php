e2c2c89dca3c4aa8a930963984eb54bdafab0936
kai0310 <kn0310@outlook.jp>
2022-04-09 22:35:22 +0900

    refactor: use translation file

--- app/Exceptions/CannotDeletePostException.php
+++ app/Exceptions/CannotDeletePostException.php
@@ -8,6 +8,6 @@ class CannotDeletePostException extends Exception
 {
     public static function notYourPost(): self
     {
-        return new self('他人の投稿を削除することはできません');
+        return new self(__('他人の投稿を削除することはできません'));
     }
 }
