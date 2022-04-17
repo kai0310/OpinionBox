5a38d8dca7df46b4d5459c67c2f4c12fcb0200f4
kai0310 <kn0310@outlook.jp>
2022-04-17 16:41:34 +0900

    style: redesign

--- resources/views/components/admin/header-nav-link.blade.php
+++ resources/views/components/admin/header-nav-link.blade.php
@@ -3,16 +3,20 @@
 @endphp
 
 <ul class="mt-1 hover:bg-gray-100 transition delay-75">
-    <li class="relative px-6 py-3">
-        @if ($active)
-            <span class="absolute inset-y-0 right-0 w-1 bg-blue-500 rounded-tl-lg rounded-bl-lg" aria-hidden="true"></span>
-        @endif
+    <li class="relative px-8 py-3">
         <a
-            class="inline-flex items-center w-full text-sm text-gray-600 transition-colors duration-150 select-none dark:hover:text-gray-200 dark:text-gray-100 @if ($active) text-blue-500  @endif"
+            @class([
+                'inline-flex items-center w-full text-sm transition-colors duration-150 select-none',
+                'text-blue-500' => $active,
+                'text-gray-600' => ! $active,
+            ])
             href="{{ route($link) }}"
         >
             {{ $icon }}
             <span class="ml-4">{{ $value }}</span>
         </a>
+        @if ($active)
+            <span class="absolute inset-y-0 right-0 w-1 bg-blue-500 rounded-tl-lg rounded-bl-lg" aria-hidden="true"></span>
+        @endif
     </li>
 </ul>
